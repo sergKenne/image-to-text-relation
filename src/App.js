43c9171421911b2dab5d674a4c5c4a51c7f09cb7
Kenne Serge <kenne_serge@mail.ru>
2020-11-14 23:33:21 +0300

    convert array to object

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 
 import './App.css';
 import React from "react";
+import data from './data.json'
+
+const users = data.user;
+
+function converseArrayToObject (users, numberPictureByPage) {
+    const numberPage = users.length/numberPictureByPage;
+    let tempArr = [];
+    let newArrayFragment = [];
+    let newObject= {};
+    
+    users.forEach(user => {
+        tempArr.push(user);
+        if(tempArr.length === numberPictureByPage) {
+            newArrayFragment.push(tempArr);
+            tempArr=[];
+        }
+    });
+    
+    newArrayFragment.forEach((item, ind) => {
+        newObject[ind] = item;
+    });
+    
+    return newObject;
+    
+}
+
+console.log("outObject:", converseArrayToObject(users, 25))
+
+// let temp = [];
+// const newArr = [];
+// let newObj = {};
+//
+// users.forEach( user => {
+//     temp.push(user);
+//     if(temp.length === 25) {
+//         newArr.push(temp);
+//         temp = [];
+//     }
+// })
+//
+// newArr.forEach((item, ind) => {
+//     newObj[ind] = item;
+// })
+//
+// console.log("newObj", newObj[1]);
+// console.log("newArr:", newArr);
+// console.log(users)
 
 function App() {
+    // console.log(data.user);
   return (
     <div className="App">
       <h1>Hello</h1>
